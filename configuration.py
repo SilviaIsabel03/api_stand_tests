58c271884e4679040d11b557d30654b0c1820918
Silvia Isabel Perez Enriquez <isabelc20@hotmail.com>
2026-09-18 23:47:03 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = 'https://cnt-703ce56f-951e-4be0-8f2b-45dae90ef086.containerhub.tripleten-services.com'
+DOC_PATH = '/docs/'
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ create_user_test.py
@@ -0,0 +1,88 @@
+import data
+import sender_stand_request
+
+
+def get_user_body(firstname):
+    current_body = data.user_body.copy()
+    current_body["firstName"] = firstname
+    return current_body
+
+def positive_assert(first_name):
+    user_body = get_user_body(first_name)
+    user_response = sender_stand_request.post_new_user(user_body)
+    assert user_response.status_code == 201
+    assert user_response.json()["authToken"] != ""
+    users_table_response = sender_stand_request.get_users_table()
+    str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
+               + user_body["address"] + ",,," + user_response.json()["authToken"]
+    assert users_table_response.text.count(str_user) == 1
+
+def negative_assert_symbol(first_name):
+    user_body = get_user_body(first_name)
+    response = sender_stand_request.post_new_user(user_body)
+    assert response.status_code == 400
+    assert response.json()["code"] == 400
+    assert response.json()["message"] == "Has introducido un nombre de usuario no válido. " \
+                                         "El nombre solo puede contener letras del alfabeto latino, " \
+                                         "la longitud debe ser de 2 a 15 caracteres."
+
+def negative_assert_no_firstname(user_body):
+    response = sender_stand_request.post_new_user(user_body)
+    assert response.status_code == 400
+    assert response.json()["code"] == 400
+    assert response.json()["message"] == "No se han aprobado todos los parámetros requeridos"
+
+# Prueba 1. Creación de un nuevo usuario o usuaria
+# El parámetro "firstName" contiene dos caracteres
+def test_create_user_2_letter_in_first_name_get_success_response():
+   positive_assert("AA")
+
+# Prueba 2. Creación de un nuevo usuario o usuaria
+# El parámetro "firstName" contiene 15 caracteres
+def test_create_user_15_letter_in_first_name_get_success_response():
+    positive_assert("Qwertyuiopalksj")
+
+# Prueba 3. Error
+# El parámetro "firstName" contiene un carácter
+def test_create_user_1_letter_in_first_name_get_error_response():
+    negative_assert_symbol("A")
+
+# Prueba 4. Error
+# El parámetro "firstName" contiene 16 caracteres
+def test_create_user_16_letter_in_first_name_get_error_response():
+    negative_assert_symbol("Agaasdasdfgretwr")
+
+# Prueba 5. Error
+# El parámetro "firstName" contiene palabras con espacios
+def test_create_user_has_space_in_first_name_get_error_response():
+    negative_assert_symbol("A Aaa")
+
+# Prueba 6. Error
+# El parámetro "firstName" contiene un string de caracteres especiales
+def test_create_user_has_special_symbol_in_first_name_get_error_response():
+    negative_assert_symbol("\"№%@\",")
+
+# Prueba 7. Error
+# El parámetro "firstName" contiene un string de números
+def test_create_user_has_number_in_first_name_get_error_response():
+    negative_assert_symbol("147")
+
+# Prueba 8. Error
+# La solicitud no contiene el parámetro "firstName"
+def test_create_user_no_first_name_get_error_response():
+    user_body = data.user_body.copy()
+    user_body.pop("firstName")
+    negative_assert_no_firstname(user_body)
+
+# Prueba 9. Error
+# El parámetro "firstName" contiene un string vacío
+def test_create_user_empty_first_name_get_error_response():
+    user_body = get_user_body("")
+    negative_assert_no_firstname(user_body)
+
+# Prueba 10. Error
+# El tipo del parámetro "firstName" es un número
+def test_create_user_number_type_first_name_get_error_response():
+    user_body = get_user_body(12)
+    response = sender_stand_request.post_new_user(user_body)
+    assert response.status_code == 400
